7e3c9bffed6dd753ad65bcad130c3fb8246d5f7a
21013198 <21013198@10.174.115.27>
2022-07-20 12:53:07 +0800

    refactor toString() method

--- ResourceCentre_Refactor/src/Camcorder.java
+++ ResourceCentre_Refactor/src/Camcorder.java
@@ -18,5 +18,6 @@ public class Camcorder extends Item{
 		
 		return output;
 	}
+	
 }
 
--- ResourceCentre_Refactor/src/Chromebook.java
+++ ResourceCentre_Refactor/src/Chromebook.java
@@ -16,6 +16,7 @@ public class Chromebook extends Item{
 		// Write your codes here
 		output = String.format("%-63s %-20s", output, os);
 		return output;
+		
 	}
 }
 
